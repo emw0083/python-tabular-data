4529db2f69043b885ee515d6cf208675e72b89dc
Emily M. Wilkins <emw0083@auburn.edu>
2022-03-16 09:53:27 -0500

    adding linear regression to scatter plots

--- tabular_data.py
+++ tabular_data.py
@@ -4,8 +4,10 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
-def ________
+
+#def ________
 """
 Makes a scatter plot that shows the linear regression of sepal length and petal length of various Iris species.
 
@@ -15,22 +17,43 @@ Makes a scatter plot that shows the linear regression of sepal length and petal 
 dataframe = pd.read_csv("iris.csv")
 #----------Scatter plot for setosa species-----------
 setosa = dataframe[dataframe.species == "Iris_setosa"]
-plt.scatter(setosa.petal_length_cm, setosa.sepal_length_cm)
+x = setosa.petal_length_cm
+y = setosa.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
 plt.xlabel("Petal length (cm)")
 plt.ylabel("Sepal length (cm)")
-plt.savefig("setosa_petal_v_sepal_length.png")
+plt.legend()
+plt.savefig("setosa_petal_v_sepal_length_regress.png")
+
 
 #---------Scatter plot for versicolor species-------------
 versicolor = dataframe[dataframe.species == "Iris_versicolor"]
-plt.scatter(versicolor.petal_length_cm, versicolor.sepal_length_cm)
+x = versicolor.petal_length_cm
+y = versicolor.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
 plt.xlabel("Petal length (cm)")
 plt.ylabel("Sepal length (cm)")
-plt.savefig("versicolor_petal_v_sepal_length.png")
+plt.legend()
+plt.savefig("versicolor_petal_v_sepal_length_regress.png")
 
 #---------Scatter plot for virginicia species-------------
 virginicia = dataframe[dataframe.species == "Iris_virginicia"]
-plt.scatter(virginicia.petal_length_cm, virginicia.sepal_length_cm)
+x = virginicia.petal_length_cm
+y = virginicia.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
 plt.xlabel("Petal length (cm)")
 plt.ylabel("Sepal length (cm)")
-plt.savefig("virginicia_petal_v_sepal_length.png")
-
+plt.legend()
+plt.savefig("virginicia_petal_v_sepal_length_regress.png")
