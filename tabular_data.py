31bbdc92474f16ffe953e0a8369e403799c0a385
Emily M. Wilkins <emw0083@auburn.edu>
2022-03-16 09:48:18 -0500

    code for other two scatter plots

--- tabular_data.py
+++ tabular_data.py
@@ -13,16 +13,24 @@ Makes a scatter plot that shows the linear regression of sepal length and petal 
 
 
 dataframe = pd.read_csv("iris.csv")
-#Scatter plot for setosa species
+#----------Scatter plot for setosa species-----------
 setosa = dataframe[dataframe.species == "Iris_setosa"]
 plt.scatter(setosa.petal_length_cm, setosa.sepal_length_cm)
 plt.xlabel("Petal length (cm)")
 plt.ylabel("Sepal length (cm)")
 plt.savefig("setosa_petal_v_sepal_length.png")
 
-
-
+#---------Scatter plot for versicolor species-------------
 versicolor = dataframe[dataframe.species == "Iris_versicolor"]
-virginicia = dataframe[dataframe.species == "Iris_virginicia"]
+plt.scatter(versicolor.petal_length_cm, versicolor.sepal_length_cm)
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.savefig("versicolor_petal_v_sepal_length.png")
 
+#---------Scatter plot for virginicia species-------------
+virginicia = dataframe[dataframe.species == "Iris_virginicia"]
+plt.scatter(virginicia.petal_length_cm, virginicia.sepal_length_cm)
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.savefig("virginicia_petal_v_sepal_length.png")
 
